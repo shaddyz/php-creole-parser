f8cdbb0e8729a3a12468eaf3e8c7b8e9db4c198c
Shaddy Zeineddine <shaddy@chunkofwood.com>
2013-04-25 10:33:48 -0700

    updated composer file for packagist compatibility, added license boiler plate

--- src/Creole/Page.php
+++ src/Creole/Page.php
@@ -1,4 +1,28 @@
 <?php
+/**
+* Creole parser and converter written in PHP
+*
+* Copyright 2012 Shaddy Zeineddine
+*
+* This file .
+*
+* Steam is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* Steam is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+* @copyright 2012 Shaddy Zeineddine
+* @license http://www.gnu.org/licenses/gpl.txt GPL v3 or later
+* @link https://github.com/shaddyz/php-creole-parser
+*/
 
 namespace Creole;
 
--- src/Creole/TextLine.php
+++ src/Creole/TextLine.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * This file is part of Shaddy Zeineddine's Creole Parser.
+ *
+ * WerkBox is free software; you can redistribute it and/or modify it under the terms of version 3 of the GNU General
+ * Public License as published by the Free Software Foundation.
+ *
+ * WerkBox is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2012 Shaddy Zeineddine
+ * @license http://www.gnu.org/licenses/gpl.txt GPL v3
+ * @link https://github.com/shaddyz/php-creole-parser
+ */
 
 namespace Creole;
 
